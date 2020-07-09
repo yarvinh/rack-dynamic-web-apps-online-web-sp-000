3cf08c5f4c9e2318b6c2120f3e99f4e1c16d6d77
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-09 16:52:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,9 @@ num_2 = Kernel.rand(1..20)
 num_3 = Kernel.rand(1..20)
 
 if num_1==num_2 && num_2==num_3
-  puts "You Win"
+  write "You Win"
 else
-  puts "You Lose"
+  write "You Lose"
 end
   end
 
